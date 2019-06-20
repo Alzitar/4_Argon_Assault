e9b54e23aeedaca4d000a234f881bcf027a8cde7
Tim Makkelie <timmakkelie@gmail.com>
2019-06-20 11:29:34 +0200

    Skybox + PlayerShip + MusicPlayer

---
+++ Assets/Scripts/Music_Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music_Player : MonoBehaviour
+{   void Awake()
+    {
+        Object.DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
